10923b5cffa574b6aed1999e7c1c8a8999faec0f
Genevieve Strittmatter <eastrittmatter@gmail.com>
2024-01-24 17:31:09 -0500

    fixed page layout and  added ability to click thumbnails

--- index.js
+++ index.js
@@ -12,67 +12,50 @@ const configObj = {
 document.addEventListener('DOMContentLoaded', () => {
     const newImg = document.createElement('img');
     document.getElementById('main-artwork').append(newImg);
-    getImage();
+    getRandImage();
 
     const newImgBtn = document.getElementById('new-img-btn');
-    newImgBtn.addEventListener('click', () => getImage());
+    newImgBtn.addEventListener('click', () => getRandImage());
 
     fetch(jsonUrl)
     .then(resp => resp.json())
     .then(images => {
-        for (image of images) {createThumbnail(image);}
+        for (let image of images) {createThumbnail(image);}
     });
 })
 
-function getImage() {
+function getRandImage() {
     fetch(`${base_Url}photos/random`, configObj)
     .then(resp => resp.json())
-    .then(imageObj => {renderImage(imageObj)});
+    .then(imageObj => {renderImage(imageObj)})
+    .catch(() => {document.querySelector('#main-artwork img').src = 'placeholder.jpg';});
 }
 
+// making this variable and this function global allow removeEventListener in renderImage to find the right function instance
 let selectedImageObj;
 function forListener() {handleLike(selectedImageObj)}
 
 function renderImage(imageObj) {
     selectedImageObj = imageObj;
     const mainArtwork = document.getElementById('main-artwork').classList;
-    if (mainArtwork != '') {mainArtwork.remove(...mainArtwork)}
-    mainArtwork.add(imageObj.id)
+    const id = imageObj.id;
+    if (mainArtwork != '') {mainArtwork.remove(...mainArtwork);}
+    mainArtwork.add(id);
     document.querySelector('#main-artwork img').src = imageObj.urls.raw + `&fit=clip&w=750&h=750`;
    
     likeBtn = document.getElementById("like-btn");
     likeBtn.textContent = emptyHeart;
-    const id = document.getElementById('main-artwork').classList[0];
     
     fetch(jsonUrl)
     .then(resp => resp.json())
     .then((images) => {
-        const foundAnImg = images.find((image) => image.id === id);
-        if (foundAnImg) {document.getElementById('num-likes').textContent = image.likes;}
+        const matchingImg = images.find((image) => image.id === id);
+        if (matchingImg) {document.getElementById('num-likes').textContent = matchingImg.likes;}
         else {document.getElementById('num-likes').textContent = 0;}
-        // the below code was simplified into the .find method
-        // let foundAnImg = false; 
-        // for (image of images) {
-        //     if (image.id === id) {
-        //         foundAnImg = true;
-        //         document.getElementById('num-likes').textContent = image.likes
-        //         break //prevents unecessary iterations after a match is found
-        //     }
-        // if (!foundAnImg) {document.getElementById('num-likes').textContent = 0}
-        // }
     });
 
     likeBtn.removeEventListener('click', forListener);
     likeBtn.addEventListener('click', forListener);
-        
-        //check if id is in db.json, 
-        //if no, post to db.json and create thumbnail of img
-        //if yes, patch and change like count
-        //update DOM regardless
-}
-
-function handleThumbnailClick() {
-    //this will render the image with the corresponding ID in the main-artwork area
 }
 
 function createThumbnail(newThumbnail) {
@@ -82,19 +65,25 @@ function createThumbnail(newThumbnail) {
     const newThumbnailDiv = document.createElement('div');
     newThumbnailImg.src = newThumbnail.image;
     newThumbnailDiv.classList.add(newThumbnail.id);
-    //newThumbnailImg.addEventListener('click', (e) => handleThumbnailClick(e));
+    newThumbnailImg.addEventListener('click', (e) => handleThumbnailClick(e));
     thumbnailTab.prepend(newThumbnailDiv);
     document.querySelector(`[id='thumbnail-tab'] div[class='${newThumbnail.id}']`).append(newThumbnailImg);
-    
-    // thumbnailTab.prepend(newThumbnailImg);
+}
+
+function handleThumbnailClick(e) {
+    //this renders the image in the main-artwork area
+    const clickedThumbnail = e.target;
+    const id = clickedThumbnail.parentNode.classList[0]
+    fetch(`${base_Url}photos/${id}`, configObj)
+    .then(resp => resp.json())
+    .then(image => renderImage(image))
+    .catch(err => window.alert(err.message));
 }
 
 function handleLike(imageObj) {
     fetch(jsonUrl)
     .then(resp => resp.json())
     .then((images) => {
-        // commented code in handleLike is being replaced by a find method
-        // let foundAnImg = false;
         const oldLikes = parseInt(document.getElementById('num-likes').textContent);
         const newLikes = oldLikes + 1;
         const foundAnImg = images.find(image => image.id === imageObj.id)
@@ -131,45 +120,4 @@ function handleLike(imageObj) {
             });
         }
     })
-        // for (image of images) {
-        //     if (image.id === imageObj.id) {
-        //         foundAnImg = true;
-        //         fetch(`${jsonUrl}${imageObj.id}`, {
-        //             method: "PATCH",
-        //             headers: {
-        //                 "Content-Type": "application/json",
-        //                 "Accept": "application/json"
-        //             },
-        //             body: JSON.stringify({
-        //                 "likes": newLikes
-        //                 //might not work as intended
-        //             })
-        //         })
-        //         document.getElementById('num-likes').textContent = newLikes;
-        //         break //prevents unecessary iterations after a match is found
-        //     }
-        // }
-        // if (!foundAnImg) { 
-        //     fetch(jsonUrl, {
-        //         method: "POST",
-        //         headers: {
-        //             "Content-Type": "application/json",
-        //             "Accept": "application/json"
-        //         },
-        //         body: JSON.stringify({
-        //             'id': imageObj.id,
-        //             'image': imageObj.urls.raw + `&fit=clip&w=100&h=100`,
-        //             'likes': 1,
-        //             'comments': [] 
-        //         })
-        //     })
-        //     .then(resp => resp.json())
-        //     .then(newThumbnail => {
-        //         document.getElementById('num-likes').textContent = 1;
-        //         createThumbnail(newThumbnail);
-        //     });
-        // }
-    // })
 }
-      
-    
